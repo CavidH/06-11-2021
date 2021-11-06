305aeae8aaa77f272dbf3cdbe1e5e3d7681537c5
CavidH <hcavid386@gmail.com>
2021-11-06 16:53:38 +0400

    Add project files.

---
+++ 06-11-2021/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _06_11_2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /**
+            Console.WriteLine("Hello World!");
+            User u = new User();
+            u.Name = "sfdf";
+            **/
+
+        }
+    }
+}
---
+++ 06-11-2021/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _06_11_2021
+{
+    class User
+    {
+       internal string Name;
+        string Surname;
+        int Age;
+
+    }
+}
